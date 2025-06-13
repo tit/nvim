3fa9867890be9e304fef0be7649267c40345b1dc
tit <sblohin@yandex.ru>
2025-06-13 12:57:06 +0300

    refactor

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+ 
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", 
+    "muniftanjim/nui.nvim",
+  },
+  
+  lazy = false, 
+  
+  opts = {},
+}
