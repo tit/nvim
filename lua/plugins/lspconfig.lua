9bf02925bbf0162801f81a4bcc0d644cf9a66933
tit <sblohin@yandex.ru>
2025-06-14 00:10:50 +0300

    refactor

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,21 @@
 return {
-  { 
+  {
     "neovim/nvim-lspconfig",
 
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+
+      lspconfig.lua_ls.setup({
+        settings = {
+			    Lua = {
+    				diagnostics = {
+					    globals = {
+                "vim",
+              },
+    				},
+		    	},
+    		},
+      })
     end,
   },
 }
