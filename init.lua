e93486e3fadd6ff71e5ba95eb80256701bd9b01f
tit <sblohin@yandex.ru>
2025-06-15 12:34:31 +0300

    refactor

--- init.lua
+++ init.lua
@@ -1,69 +1,9 @@
+-- do not sort below
+
 require("core.globals")
-
--- region lazy
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-
-	local out = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazyrepo,
-    lazypath,
-  })
-
-  if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{
-        "Failed to clone lazy.nvim:\n",
-        "ErrorMsg"
-      },
-			{
-        out,
-        "WarningMsg"
-      },
-			{
-        "\nPress any key to exit..."
-      },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    {
-      import = "plugins"
-    },
-  },
-})
-
--- endregion
-
+require("core.lazy")
 require("core.folding")
 require("core.key_mapping")
 require("core.options")
+require("core.other")
 
-vim.diagnostic.config({
-        signs = {
-                text = {
-                        [vim.diagnostic.severity.ERROR] = " ",
-                        [vim.diagnostic.severity.WARN] = " ",
-                        [vim.diagnostic.severity.INFO] = "󰋼 ",
-                        [vim.diagnostic.severity.HINT] = "󰌵 ",
-                },
-                numhl = {
-                        [vim.diagnostic.severity.ERROR] = "",
-                        [vim.diagnostic.severity.WARN] = "",
-                        [vim.diagnostic.severity.HINT] = "",
-                        [vim.diagnostic.severity.INFO] = "",
-                },
-        },
-})
---
+++ lua/core/other.lua
@@ -0,0 +1,16 @@
+vim.diagnostic.config({
+        signs = {
+                text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.INFO] = "󰋼 ",
+                        [vim.diagnostic.severity.HINT] = "󰌵 ",
+                },
+                numhl = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN] = "",
+                        [vim.diagnostic.severity.HINT] = "",
+                        [vim.diagnostic.severity.INFO] = "",
+                },
+        },
+})
