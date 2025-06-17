62d06a612b2ed425afbcc3cf88556dac2ed512f7
tit <sblohin@yandex.ru>
2025-06-17 20:56:04 +0300

    refactor

--- lua/core/other.lua
+++ lua/core/other.lua
@@ -1,16 +1,10 @@
 vim.diagnostic.config({
-        signs = {
-                text = {
-                        [vim.diagnostic.severity.ERROR] = " ",
-                        [vim.diagnostic.severity.WARN] = " ",
-                        [vim.diagnostic.severity.INFO] = "󰋼 ",
-                        [vim.diagnostic.severity.HINT] = "󰌵 ",
-                },
-                numhl = {
-                        [vim.diagnostic.severity.ERROR] = "",
-                        [vim.diagnostic.severity.WARN] = "",
-                        [vim.diagnostic.severity.HINT] = "",
-                        [vim.diagnostic.severity.INFO] = "",
-                },
-        },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = "󰋼 ",
+      [vim.diagnostic.severity.HINT] = "󰌵 ",
+    },
+  },
 })
